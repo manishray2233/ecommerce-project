97cfccc3e2b67f2a03696870a3c4e7b66073530c
Manish Kumar Ray <manishray2233@gmail.com>
2023-09-20 11:20:10 +0530

    Started working on ecommerce-project

---
+++ api/index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const crypto = require("crypto");
+const nodemailer = require("nodemailer");
+
+const app = express();
+const port = 8001;
+const cors = require("cors");
+app.use(cors());
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+const jwt = require("jsonwebtoken");
+
+mongoose
+  .connect(
+    "mongodb+srv://manishray2233:manish@cluster0.xfybsve.mongodb.net/",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log("Error connecting to MongoDB", err);
+  });
+
+app.listen(port, () => {
+  console.log("Server is running on port " + port);
+});
