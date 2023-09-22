ddbdbb3917085b39dfb09a7d379a2238708e9b4a
Manish Kumar Ray <manishray2233@gmail.com>
2023-09-22 20:09:19 +0530

    ProductInfoScreen

---
+++ screens/ProductInfoScreen.js
@@ -0,0 +1,167 @@
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  Platform,
+  ScrollView,
+  Pressable,
+  TextInput,
+  Image,
+  ImageBackground,
+  Dimensions,
+} from "react-native";
+import React, { useState, useEffect, useCallback, useContext } from "react";
+import {
+  Feather,
+  Ionicons,
+  MaterialCommunityIcons,
+  Entypo,
+  AntDesign,
+} from "@expo/vector-icons";
+import { SliderBox } from "react-native-image-slider-box";
+import axios from "axios";
+import { useRoute, useNavigation } from "@react-navigation/native";
+
+const ProductInfoScreen = () => {
+  const route = useRoute();
+  const { width } = Dimensions.get("window");
+  const height = (width * 100) / 100;
+  const navigation = useNavigation();
+  return (
+    <ScrollView
+      style={{ flex: 1, backgroundColor: "white" }}
+      showsVerticalScrollIndicator={false}
+    >
+      <View
+        style={{
+          backgroundColor: "#00CED1",
+          padding: 10,
+          flexDirection: "row",
+          alignItems: "center",
+          paddingTop: 50,
+        }}
+      >
+        <Pressable
+          style={{
+            flexDirection: "row",
+            alignItems: "center",
+            marginHorizontal: 7,
+            gap: 10,
+            backgroundColor: "white",
+            borderRadius: 3,
+            height: 38,
+            flex: 1,
+          }}
+        >
+          <AntDesign
+            style={{ paddingLeft: 10 }}
+            name="search1"
+            size={22}
+            color="black"
+          />
+          <TextInput placeholder="Search Amazon.in" />
+        </Pressable>
+
+        <Feather name="mic" size={24} color="black" />
+      </View>
+
+      <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+        {route.params.carouselImages.map((item, index) => (
+          <ImageBackground
+            style={{ width, height, marginTop: 25, resizeMode: "contain" }}
+            source={{ uri: item }}
+            key={index}
+          >
+            <View
+              style={{
+                padding: 20,
+                flexDirection: "row",
+                alignItems: "center",
+                justifyContent: "space-between",
+              }}
+            >
+              <View
+                style={{
+                  width: 40,
+                  height: 40,
+                  borderRadius: 20,
+                  backgroundColor: "#C60C30",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  flexDirection: "row",
+                }}
+              >
+                <Text
+                  style={{
+                    color: "white",
+                    textAlign: "center",
+                    fontWeight: "600",
+                    fontSize: 12,
+                  }}
+                >
+                  20% off
+                </Text>
+              </View>
+
+              <View
+                style={{
+                  width: 40,
+                  height: 40,
+                  borderRadius: 20,
+                  backgroundColor: "#E0E0E0",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  flexDirection: "row",
+                }}
+              >
+                <MaterialCommunityIcons
+                  name="share-variant"
+                  size={24}
+                  color="black"
+                />
+              </View>
+            </View>
+
+            <View
+              style={{
+                width: 40,
+                height: 40,
+                borderRadius: 20,
+                backgroundColor: "#E0E0E0",
+                justifyContent: "center",
+                alignItems: "center",
+                flexDirection: "row",
+                marginTop: "auto",
+                marginLeft: 20,
+                marginBottom: 20,
+              }}
+            >
+              <AntDesign name="hearto" size={24} color="black" />
+            </View>
+          </ImageBackground>
+        ))}
+      </ScrollView>
+
+      <View style={{ padding: 10 }}>
+        <Text style={{ fontSize: 15, fontWeight: "500" }}>
+          {route?.params?.title}
+        </Text>
+        <Text style={{ fontSize: 18, fontWeight: "600", marginTop: 6 }}>
+          ₹{route?.params?.price}
+        </Text>
+      </View>
+
+      <Text style={{ height: 1, borderColor: "#D0D0D0", borderWidth: 1 }} />
+
+      <View style={{ flexDirection: "row", alignItems: "center", padding: 10 }}>
+        <Text>Color: </Text>
+        <Text>{route?.params?.color}</Text>
+      </View>
+    </ScrollView>
+  );
+};
+
+export default ProductInfoScreen;
+
+const styles = StyleSheet.create({});
